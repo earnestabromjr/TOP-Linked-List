b5bfa8c8473fed7bff8c757aab773f85facaf157
earnestabromjr <abromearnest@gmail.com>
2025-12-31 17:21:21 -0600

    Init commit

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.nextNode = null;
+	}
+}
+export class LinkedList {
+	constructor(node) {
+		this.node = node;
+		this.value = node.value;
+	}
+
+	append = (value) => {};
+}
