a1e8abd36421236b6acf3ec220fae56e4ea334e9
earnestabromjr <abromearnest@gmail.com>
2025-12-31 18:13:51 -0600

    Added append prepend size methods to LinkedList class

--- linkedList.js
+++ linkedList.js
@@ -1,14 +1,44 @@
 class Node {
-	constructor(value) {
+	constructor(value = null) {
 		this.value = value;
 		this.nextNode = null;
 	}
 }
 export class LinkedList {
-	constructor(node) {
-		this.node = node;
-		this.value = node.value;
+	constructor(head = null) {
+		this.head = head;
 	}
 
-	append = (value) => {};
+	append = (value) => {
+		let node = this.head;
+		if (node.nextNode == null) {
+			this.head = new Node(value);
+			return;
+		}
+		while (node.nextNode) {
+			if (node.nextNode == null) {
+				let lastNode = new Node(value);
+				node.nextNode = lastNode;
+				return;
+			}
+		}
+	};
+
+	prepend = (value) => {
+		let newHead = new Node(value);
+		let oldHead = this.head;
+		newHead.nextNode = oldHead;
+		this.head = newHead;
+		return;
+	};
+
+	size = () => {
+		let count = 0;
+		let node = this.head;
+		while (node) {
+			count += 1;
+			node = node.nextNode;
+		}
+		return count;
+	};
 }
